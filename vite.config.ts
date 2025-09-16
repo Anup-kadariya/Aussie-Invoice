020f2c3637c1d2f272697a14f6b6f68513af06b1
Anup <53438855+Anup-kadariya@users.noreply.github.com>
2025-09-16 13:50:25 +0930

    Update vite.config.ts
    added with repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/aussie-invoice/', // repo name to deploy using github
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
